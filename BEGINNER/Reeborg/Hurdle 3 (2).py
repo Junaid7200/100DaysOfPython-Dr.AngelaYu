e5eb34467a4b908fe68b76d85ac90b4c9ef786fa
Junaid7200 <junaidjaffery1@gmail.com>
2026-01-01 21:18:53 +0500

    Add Reeborg Hurdle 3 variation 2

---
+++ BEGINNER/Reeborg/Hurdle 3 (2).py
@@ -0,0 +1,20 @@
+def turner():
+    turn_left()
+    turn_left()
+    turn_left()
+    move()
+    
+while not at_goal():
+    
+    move()
+    turn_left()
+    move()
+    turner()
+    turner()
+    turn_left()
+
+    
+################################################################
+# WARNING: Do not change this comment.
+# Library Code is below.
+################################################################
