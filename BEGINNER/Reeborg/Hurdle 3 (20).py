b1e91ac85637c52a63ac6eecab30cae7516c1b91
Junaid7200 <junaidjaffery1@gmail.com>
2026-01-01 21:18:56 +0500

    Add Reeborg Hurdle 3 variation 20

---
+++ BEGINNER/Reeborg/Hurdle 3 (20).py
@@ -0,0 +1,25 @@
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+    
+def circular(): # if 
+    turn_right()
+    move()
+    turn_right()
+    move()
+    
+    
+while not is_goal():
+    if wall_in_front():
+        turn_left
+        if front_is_clear():
+            move()
+            
+
+
+    
+################################################################
+# WARNING: Do not change this comment.
+# Library Code is below.
+################################################################
