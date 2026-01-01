974fc29a9550e50acaa92ef2547ee658824a8aff
Junaid7200 <junaidjaffery1@gmail.com>
2026-01-01 21:18:57 +0500

    Add Reeborg Hurdle 4 variation 1

---
+++ BEGINNER/Reeborg/Hurdle 4 (1).py
@@ -0,0 +1,57 @@
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+    
+def Checkleft():
+    turn_left()
+    if not wall_in_front():
+        turn_right()
+        return True
+    else:
+        turn_right()
+        return False
+def Checkright():
+    turn_right()
+    if not wall_in_front():
+        turn_left()
+        return True
+    else:
+        turn_left()
+        return False
+
+def Clearleft():
+    turn_left()
+    if front_is_clear():
+        turn_right()
+        return True
+    else:
+        turn_right()
+        return False
+def Clearright():
+    turn_right()
+    if front_is_clear():
+        turn_left()
+        return True
+    else:
+        turn_left()
+        return False
+def jump():
+    turn_left()
+    move()
+    if Checkright():    
+        turn_right()
+        move()
+        turn_right()
+        move()
+        turn_left()
+    
+    
+while not at_goal():
+    
+
+
+################################################################
+# WARNING: Do not change this comment.
+# Library Code is below.
+################################################################
