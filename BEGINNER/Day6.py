205b5336cd3428363440dd49bffc58c0eecead5f
Junaid7200 <junaidjaffery1@gmail.com>
2026-01-01 21:18:50 +0500

    Add Day 6: Functions and code blocks

---
+++ BEGINNER/Day6.py
@@ -0,0 +1,12 @@
+# june 22, 2024
+# while loops and functions and reeborg
+
+# https://reeborg.ca/reeborg.html?lang=en&mode=python&menu=worlds%2Fmenus%2Freeborg_intro_en.json&name=Hurdle%201&url=worlds%2Ftutorial_en%2Fhurdle1.json
+
+# reminder: indentation matters
+
+# https://reeborg.ca/reeborg.html?lang=en&mode=python&menu=worlds%2Fmenus%2Freeborg_intro_en.json&name=Hurdle%203&url=worlds%2Ftutorial_en%2Fhurdle3.json
+
+# https://reeborg.ca/reeborg.html?lang=en&mode=python&menu=worlds%2Fmenus%2Freeborg_intro_en.json&name=Hurdle%204&url=worlds%2Ftutorial_en%2Fhurdle4.json
+
+# it was all hurdles and reeborg, did everything there
