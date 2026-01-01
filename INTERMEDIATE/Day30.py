bf18861fa14d2e9f6c11c7fd7be5af127fe56028
Junaid7200 <junaidjaffery1@gmail.com>
2026-01-01 21:29:23 +0500

    Add Day 30: Error handling and exceptions

---
+++ INTERMEDIATE/Day30.py
@@ -0,0 +1,31 @@
+# July 8, 2024
+
+# try:
+#     file = open("filey.txt")
+# except FileNotFoundError as msg:
+#     print(f"{msg} was not found") # if try fails
+# else:
+#     print("File was found") # if try succeeds
+# finally:
+#     print("File was closed") # always runs
+
+# height = float(input("height: "))
+# weight = int(input("Weight: "))
+
+# if height > 3:
+#     raise ValueError("Human height should not be over 3 meters.")
+
+# bmi = weight / height ** 2
+# fruits = ["apple", "pumpkin", "orange"]
+# def makePie(index):
+#     try:
+#         fruit = fruits[index]
+#     except IndexError:
+#         print(f"The index is out of range")
+#         fruit = "Fruit"
+#         print(fruit + "pie")
+#     else:
+#         print(fruit + "pie")
+
+
+# makePie(3) 
