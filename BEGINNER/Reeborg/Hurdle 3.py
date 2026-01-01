4d11fe9f0bf34f963f773fcf91e1e1bc62b85a77
Junaid7200 <junaidjaffery1@gmail.com>
2026-01-01 21:18:52 +0500

    Add Reeborg Hurdle 3 base exercise

---
+++ BEGINNER/Reeborg/Hurdle 3.py
@@ -0,0 +1,19 @@
+def turner():
+    turn_left()
+    turn_left()
+    turn_left()
+    move()
+    
+while at_goal():
+    move()
+    turn_left()
+    move()
+    turner()
+    turner()
+    turn_left()
+
+    
+################################################################
+# WARNING: Do not change this comment.
+# Library Code is below.
+################################################################
