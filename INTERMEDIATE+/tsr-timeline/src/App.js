b64bf4629b3ea3b16a257e385ee00d6ee5d91624
Junaid7200 <junaidjaffery1@gmail.com>
2026-01-01 21:36:40 +0500

    Add TSR timeline app main component

---
+++ INTERMEDIATE+/tsr-timeline/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import TSRTimeline from './TSRTimeline';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <TSRTimeline />
+      </header>
+    </div>
+  );
+}
+
+export default App;
